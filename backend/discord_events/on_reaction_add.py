c6c8760d662fa681524e2e8c52254748c198cc96
Lagicrus <Lagicrus@users.noreply.github.com>
2018-10-01 23:04:32 +0100

    attempt at posting image

--- backend/discord_events/on_reaction_add.py
+++ backend/discord_events/on_reaction_add.py
@@ -5,6 +5,13 @@ import logging
 log = logging.getLogger(__name__)
 
 
+async def inktober_post(message: discord.Message, bot, bot_spam):
+    embed = discord.Embed(title="Inktober", timestamp=message.timestamp)
+    embed.set_image(url=message.attachments[0]["proxy_url"])
+    embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
+    await bot.send_message(bot_spam, embed=embed)
+
+
 class on_reaction_event:
     def __init__(self, bot):
         self.bot = bot
@@ -14,13 +21,19 @@ class on_reaction_event:
             log.info("Is in blob")
             if reaction.message.channel.id == "493851049942319114":
                 log.info("channel")
-                if await backend.helpers.user_role_authed(user):
-                    if reaction.custom_emoji:
-                        if reaction.emoji.name.lower() in ["greentick", "green_tick"]:
-                            log.info("Got message {}".format(reaction.message.id))
-                            log.info(reaction.message.attachments)
-                            log.info(reaction.message.attachments[0]["proxy_url"])
+                if reaction.message.attachments != []:
+                    if await backend.helpers.user_role_authed(user):
+                        if reaction.custom_emoji:
+                            if reaction.emoji.name.lower() in ["greentick", "green_tick"]:
+                                log.info("Got message {}".format(reaction.message.id))
+                                log.info(reaction.message.attachments)
+                                log.info(reaction.message.attachments[0]["proxy_url"])
 
+                                bot_spam = reaction.message.server.get_channel("411929226066001930")
+
+                                await inktober_post(reaction.message, self.bot, bot_spam)
+                else:
+                    log.info("No attachments")
 
 def setup(bot):
     bot.add_cog(on_reaction_event(bot))
